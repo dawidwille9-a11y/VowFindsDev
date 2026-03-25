881b4a2ea931074a4f392fa275ba15cff389ca0c
dawidwille9-a11y <dawidwille9@gmail.com>
2026-03-25 08:52:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    minify: 'esbuild',
+  },
+  esbuild: {
+    jsx: 'automatic',
+  },
 })
